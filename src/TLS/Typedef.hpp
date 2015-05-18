93d74e202de8c380d298b609faff1eba805c8d4d
Juan Pablo Garibotti Arias <jp.garibotti@gmail.com>
2015-05-17 22:59:37 -0300

    + Added const qualifier to cast operator. + Added == and != operators.

--- src/TLS/Typedef.hpp
+++ src/TLS/Typedef.hpp
@@ -14,7 +14,16 @@ namespace tls
   struct Typedef
   {
     Type value;
-    explicit operator Type(){ return value; }
+    explicit operator Type() const { return value; }
+
+    friend bool operator==(Typedef const& lhs, Typedef const& rhs)
+    {
+      return lhs.value == rhs.value;
+    }
+    friend bool operator!=(Typedef const& lhs, Typedef const& rhs)
+    {
+      return !operator==(lhs,rhs);
+    }
   };
 }
 
